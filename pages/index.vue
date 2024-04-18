e6611d1555bcd9219d3c4de9cb7c21d34a4761f4
Preston Bruce <pb.itoldyouso@gmail.com>
2024-04-17 21:07:32 -0500

    Home Page Update

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,30 @@
 <template>
     <div class="flex justify-center">
         <VitePwaManifest />
-            <h1 class="font-sans text-5xl content-center">More than Farming</h1>
+        <h1 class="font-sans text-5xl content-center">More than Farming</h1>
+    </div>
+    <div class="flex justify-center">
+        <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">
+                    <NuxtLink to="/agegroups/5-10">Activities for 5-10 year old</NuxtLink>
+                </h2>
+            </div>
+        </div>
+        <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">
+                    <NuxtLink to="/agegroups/11-15">Activities for 11-15 year old</NuxtLink>
+                </h2>
+            </div>
+        </div>
+        <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">
+                    <NuxtLink to="/agegroups/16-20">Activities for 16+ year old</NuxtLink>
+                </h2>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,6 +32,4 @@
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
